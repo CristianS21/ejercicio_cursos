ad1deb7555959e4429cd043bd5550bfd0178a876
CristianSaccani <csaccani86@gmail.com>
2022-09-03 15:58:31 -0300

    página inicio

--- django_/django_/urls.py
+++ django_/django_/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     #path('', admin.site.urls),
     #path('', probar_plantilla),
-    path ("app1_cursos/", include ("app1_cursos.urls")), 
+    path ("", include ("app1_cursos.urls")), 
 
 ]
 
